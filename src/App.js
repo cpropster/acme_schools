d79671c1bb5bbbd9a2bd496659b3908ea9c17da9
Chrys Propster <cpropster@gmail.com>
2020-03-12 01:02:09 -0700

    got front end working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
+import axios from "axios";
 import React from "react";
-// import qs from "qs";
+import qs from "qs";
 
-// const { useState, useEffect } = React;
+const { useState, useEffect } = React;
 
 const App = () => {
-  //   const [error, setError] = useState("");
-  //   const [schools, setSchools] = useState([]);
-  //   const [students, setStudents] = useState([]);
-  //   const [params, setParams] = useState(qs.parse(window.location.hash.slice(1)));
+  const [error, setError] = useState("");
+  const [schools, setSchools] = useState([]);
+  const [students, setStudents] = useState([]);
+  const [params, setParams] = useState(qs.parse(window.location.hash.slice(1)));
 
-  //   useEffect(() => {
-  //     window.addEventListener("hashchange", () => {
-  //       setParams(qs.parse(window.location.hash.slice(1)));
-  //     });
-  //   }, []);
+  useEffect(() => {
+    window.addEventListener("hashchange", () => {
+      setParams(qs.parse(window.location.hash.slice(1)));
+    });
+  }, []);
 
-  //   const { view } = params;
+  const { view } = params;
+
+  useEffect(() => {
+    Promise.all([axios.get("/api/schools"), axios.get("/api/students")])
+      .then((responses) => responses.map((response) => response.data))
+      .then((results) => {
+        setSchools(results[0]);
+        setStudents(results[1]);
+        setError("");
+      })
+      .catch((ex) => setError(ex.response.data.message));
+  }, []);
 
   return (
     <div>
-      <hr />
+      <h1>
+        <a href="#">ACME SCHOOLS</a>
+        <ul>
+          <li>
+            {schools.length} {schools.length === 1 ? "School" : "Schools"}
+          </li>
+          <li>
+            {students.length} {students.length === 1 ? "Student" : "Students"}
+          </li>
+        </ul>
+      </h1>
     </div>
   );
 };
